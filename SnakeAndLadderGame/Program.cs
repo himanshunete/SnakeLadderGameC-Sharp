db32d6a37c0b88ed5384276a0a4de7259eb8054f
himanshunete <himanshuneteh@gmail.com>
2021-02-03 14:22:45 +0530

    [himanshunete]Add PlayerRollsTheDice

--- SnakeAndLadderGame/Program.cs
+++ SnakeAndLadderGame/Program.cs
@@ -7,7 +7,12 @@ namespace SnakeAndLadderGame
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder Game");
-           
+            int numberOfPlayers = 1;
+            int position = 0;
+            Console.WriteLine(" Only " + numberOfPlayers + " player is playing the game, starting at position " + position + " now ");
+            Random rand = new Random();
+            int diceNumber = rand.Next(1, 7);
+            Console.WriteLine(" Player rolls the dice and the dice number he gets is: " + diceNumber);
         }
     }
 }
