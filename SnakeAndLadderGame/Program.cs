ef8e15feeda1ff441a07d849ece61e22db6ef80b
himanshunete <himanshuneteh@gmail.com>
2021-02-03 15:15:06 +0530

    [himanshunete]Add AchieveExactWinningPositionAndNotAbove

--- SnakeAndLadderGame/Program.cs
+++ SnakeAndLadderGame/Program.cs
@@ -9,7 +9,6 @@ namespace SnakeAndLadderGame
             Console.WriteLine("Welcome to Snake and Ladder Game");
             //Variables
             int numberOfPlayers = 1;
-            int position = 0;
             int numOfPlay = 1;
 
             //Constants
@@ -17,40 +16,50 @@ namespace SnakeAndLadderGame
             int Ladder = 1;
             int Snake = 2;
             int WIN_POSITION = 100;
+            int[] position = new int[2000];
 
-            Console.WriteLine(" Only " + numberOfPlayers + " player is playing the game, starting at position " + position + " now ");
+            Console.WriteLine(" Only " + numberOfPlayers + " player is playing the game at position " + position + " now ");
 
             //Computation
-            while (position != WIN_POSITION)
+            while ( position[numOfPlay] != WIN_POSITION)
             {
+                position[0] = 0;
                 Random rand = new Random();
                 int diceNumber = rand.Next(1, 7);
                 int option = rand.Next(0, 3);
+                if (position[numOfPlay] > WIN_POSITION)
+                {
+                    position[numOfPlay] = position[numOfPlay - 1];
+                }
                 if (option == NoPlay)
                 {
-                    Console.WriteLine(" The Player is at the same position " + position);
+                    position[numOfPlay + 1] = position[numOfPlay];
+                    Console.WriteLine(" The Player is at the same position " + position[numOfPlay + 1]);
                 }
                 else if (option == Ladder)
                 {
-                    position = position + diceNumber;
-                    Console.WriteLine("Player moves forward to position:" + position);
+                    position[numOfPlay + 1] = position[numOfPlay] + diceNumber;
+                    Console.WriteLine("Player moves forward to position:" + position[numOfPlay + 1]);
                 }
                 else if (option == Snake)
                 {
-                    position = position - diceNumber;
-                    if (position < 0)
+                    position[numOfPlay + 1] = position[numOfPlay] - diceNumber;
+                    if (position[numOfPlay + 1] < 0)
                     {
-                        position = 0;
+                        position[numOfPlay + 1] = 0;
                         Console.WriteLine(" Game restarts ");
                     }
-                    Console.WriteLine("Player moves back to position:" + position);
+                    Console.WriteLine("Player moves back to position:" + position[numOfPlay + 1]);
                 }
-                if (position == WIN_POSITION)
+                if (position[numOfPlay + 1] == WIN_POSITION)
                 {
                     Console.WriteLine(" The Player wins the Snake ladder game ");
                     Environment.Exit(0);
                 }
+
                 numOfPlay++;
+
+
             }
         }
     }
